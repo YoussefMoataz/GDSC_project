fa381c010c82c5ff3e0c8ec88ccb7725629ba742
Youssef Moataz <yousefcrunshy03@gmail.com>
2021-10-22 22:29:04 +0200

    Initial commit

---
+++ app/src/main/java/com/yquery/gdsc_first_project/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.yquery.gdsc_first_project
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yquery.gdsc_first_project.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.text.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+
+            intent.apply {
+                data = Uri.parse("https://play.google.com/store/apps/details?id=com.yquery.qrcodescannerandgenerator")
+            }
+
+            startActivity(intent)
+
+        }
+
+    }
+}
