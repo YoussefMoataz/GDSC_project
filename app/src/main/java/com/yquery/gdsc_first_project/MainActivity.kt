bf47ba89baa0e6f08f9ecd06460752639cc1c764
Youssef Moataz <50296580+YoussefMoataz@users.noreply.github.com>
2021-12-15 21:39:51 +0200

    Update MainActivity.kt

--- app/src/main/java/com/yquery/gdsc_first_project/MainActivity.kt
+++ app/src/main/java/com/yquery/gdsc_first_project/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding.text1.setOnClickListener {
 
             Toast.makeText(this, "Hello from FCAI-CU !", Toast.LENGTH_SHORT).show()
+            // pull
         }
 
     }
