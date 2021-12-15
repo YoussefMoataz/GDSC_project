2636b496da4f14e9d7a7e4b05e3dba769dda35fc
Youssef Moataz <yousefcrunshy03@gmail.com>
2021-12-15 21:40:29 +0200

    Test

--- app/src/main/java/com/yquery/gdsc_first_project/MainActivity.kt
+++ app/src/main/java/com/yquery/gdsc_first_project/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this, "Hello from FCAI-CU !", Toast.LENGTH_SHORT).show()
             // pull
+            // done
         }
 
     }
